6f3fcc4e97928f2c9e49418664f0cc86fb00db58
wisemuji <wisemuji@gmail.com>
2024-02-14 02:24:22 +0900

    feat: add zoom app color scheme

--- app/src/main/java/com/wisemuji/zoomclone/ui/theme/Color.kt
+++ app/src/main/java/com/wisemuji/zoomclone/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.wisemuji.zoomclone.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange = Color(0xFFFF742F)
+val Blue = Color(0xFF0E72EC)
+val Red = Color(0xFFDE2827)
+val Green = Color(0xFF4FD963)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF000000)
+val Gray80 = Color(0xFF38394D)
+val Gray60 = Color(0xFF717075)
+val Gray50 = Color(0xFF929197)
+val Gray20 = Color(0xFFEBEAEF)
+val Gray10 = Color(0xFFF9F9F9)
+val White = Color(0xFFFFFFFF)
--- app/src/main/java/com/wisemuji/zoomclone/ui/theme/Theme.kt
+++ app/src/main/java/com/wisemuji/zoomclone/ui/theme/Theme.kt
@@ -1,55 +1,39 @@
 package com.wisemuji.zoomclone.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Blue,
+    onPrimary = White,
+    secondary = Orange,
+    onSecondary = White,
+    tertiary = Green,
+    onTertiary = White,
+    background = White,
+    onBackground = Black,
+    surface = White,
+    onSurface = Black,
+    surfaceContainer = Gray10,
 )
+
+// TODO: Add dark color scheme
+private val DarkColorScheme = LightColorScheme
 
 @Composable
 fun ZoomCloneComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
