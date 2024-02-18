0eca0e26b8c26025b9e0d97809867c2e73256a88
wisemuji <wisemuji@gmail.com>
2024-02-18 19:13:54 +0900

    feat: setup Stream Video SDK

---
+++ app/src/main/java/com/wisemuji/zoomclone/ZoomApplication.kt
@@ -0,0 +1,31 @@
+package com.wisemuji.zoomclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import io.getstream.video.android.core.StreamVideo
+import io.getstream.video.android.core.StreamVideoBuilder
+import io.getstream.video.android.model.User
+
+@HiltAndroidApp
+class ZoomCloneComposeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initStreamVideo()
+    }
+
+    private fun initStreamVideo() {
+        val userId = "stream"
+        StreamVideoBuilder(
+            context = applicationContext,
+            apiKey = BuildConfig.STREAM_API_KEY,
+            token = StreamVideo.devToken(userId),
+            user = User(
+                id = userId,
+                name = "stream",
+                image = "http://placekitten.com/200/300",
+                role = "admin"
+            )
+        ).build()
+    }
+}
--- build-logic/src/main/kotlin/com/wisemuji/zoomclone/HiltAndroid.kt
+++ build-logic/src/main/kotlin/com/wisemuji/zoomclone/HiltAndroid.kt
@@ -13,6 +13,7 @@ internal fun Project.configureHiltAndroid() {
     val libs = extensions.libs
     dependencies {
         "implementation"(libs.findLibrary("hilt.android").get())
+        "implementation"(libs.findLibrary("androidx.hilt.navigation.compose").get())
         "kapt"(libs.findLibrary("hilt.android.compiler").get())
         "kaptAndroidTest"(libs.findLibrary("hilt.android.compiler").get())
     }
